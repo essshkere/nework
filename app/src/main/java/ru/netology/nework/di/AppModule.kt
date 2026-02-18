a169857e79d1b219b4af4128992d1bde93f8e874
Aydar <hispooy@gmail.com>
2026-02-18 22:33:12 +0500

    Create all class 34

--- app/src/main/java/ru/netology/nework/di/AppModule.kt
+++ app/src/main/java/ru/netology/nework/di/AppModule.kt
@@ -2,6 +2,7 @@ package ru.netology.nework.di
 
 import android.content.Context
 import androidx.room.Room
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -27,117 +28,129 @@ private const val BASE_URL = "http://94.228.125.136:8080/"
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AppModule {
+abstract class AppModule {
 
-    @Provides
-    @Singleton
-    fun provideTokenProvider(@ApplicationContext context: Context): TokenProvider =
-        TokenProviderImpl(context)
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideAuthInterceptor(tokenProvider: TokenProvider): Interceptor =
-        AuthInterceptor(tokenProvider)
+    abstract fun bindAuthRepository(impl: AuthRepositoryImpl): AuthRepository
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideOkHttpClient(authInterceptor: Interceptor): OkHttpClient {
-        val loggingInterceptor = HttpLoggingInterceptor().apply {
-            level = HttpLoggingInterceptor.Level.BODY
+    abstract fun bindTokenProvider(impl: TokenProviderImpl): TokenProvider
+
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideAuthInterceptor(tokenProvider: TokenProvider): Interceptor =
+            AuthInterceptor(tokenProvider)
+
+        @Provides
+        @Singleton
+        fun provideOkHttpClient(authInterceptor: Interceptor): OkHttpClient {
+            val loggingInterceptor = HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+            return OkHttpClient.Builder()
+                .addInterceptor(authInterceptor)
+                .addInterceptor(loggingInterceptor)
+                .build()
         }
-        return OkHttpClient.Builder()
-            .addInterceptor(authInterceptor)
-            .addInterceptor(loggingInterceptor)
-            .build()
+
+        @Provides
+        @Singleton
+        fun provideRetrofit(client: OkHttpClient): Retrofit =
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        @Provides
+        @Singleton
+        fun provideAuthApi(retrofit: Retrofit): AuthApi = retrofit.create(AuthApi::class.java)
+
+        @Provides
+        @Singleton
+        fun providePostApi(retrofit: Retrofit): PostApi = retrofit.create(PostApi::class.java)
+
+        @Provides
+        @Singleton
+        fun provideEventApi(retrofit: Retrofit): EventApi = retrofit.create(EventApi::class.java)
+
+        @Provides
+        @Singleton
+        fun provideUserApi(retrofit: Retrofit): UserApi = retrofit.create(UserApi::class.java)
+
+        @Provides
+        @Singleton
+        fun provideMediaApi(retrofit: Retrofit): MediaApi = retrofit.create(MediaApi::class.java)
+
+        @Provides
+        @Singleton
+        fun provideJobApi(retrofit: Retrofit): JobApi = retrofit.create(JobApi::class.java)
+
+        @Provides
+        @Singleton
+        fun provideDatabase(@ApplicationContext context: Context): AppDatabase =
+            Room.databaseBuilder(context, AppDatabase::class.java, "app.db")
+                .fallbackToDestructiveMigration()
+                .build()
+
+        @Provides
+        @Singleton
+        fun providePostRepository(
+            postApi: PostApi,
+            userApi: UserApi,
+            mediaApi: MediaApi,
+            db: AppDatabase
+        ): PostRepository = PostRepositoryImpl(
+            postApi = postApi,
+            userApi = userApi,
+            postDao = db.postDao(),
+            mediaApi = mediaApi,
+            appDatabase = db
+        )
+
+        @Provides
+        @Singleton
+        fun provideEventRepository(
+            eventApi: EventApi,
+            mediaApi: MediaApi,
+            db: AppDatabase
+        ): EventRepository = EventRepositoryImpl(
+            eventApi = eventApi,
+            eventDao = db.eventDao(),
+            mediaApi = mediaApi
+        )
+
+        @Provides
+        @Singleton
+        fun provideUserRepository(
+            userApi: UserApi,
+            db: AppDatabase,
+            postRepository: PostRepository
+        ): UserRepository = UserRepositoryImpl(
+            userApi = userApi,
+            userDao = db.userDao(),
+            postRepository = postRepository
+        )
+
+        @Provides
+        @Singleton
+        fun provideJobRepository(jobApi: JobApi): JobRepository = JobRepositoryImpl(jobApi)
+
+        @Provides
+        @Singleton
+        fun provideParticipantAdapter(): ParticipantAdapter = ParticipantAdapter()
+
+        @Provides
+        @Singleton
+        fun provideEventAdapter(): EventAdapter = EventAdapter()
+
+        @Provides
+        @Singleton
+        fun provideUserAdapter(): UserAdapter = UserAdapter()
     }
-
-    @Provides
-    @Singleton
-    fun provideRetrofit(client: OkHttpClient): Retrofit =
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .client(client)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-    @Provides
-    @Singleton
-    fun provideAuthApi(retrofit: Retrofit): AuthApi = retrofit.create(AuthApi::class.java)
-
-    @Provides
-    @Singleton
-    fun providePostApi(retrofit: Retrofit): PostApi = retrofit.create(PostApi::class.java)
-
-    @Provides
-    @Singleton
-    fun provideEventApi(retrofit: Retrofit): EventApi = retrofit.create(EventApi::class.java)
-
-    @Provides
-    @Singleton
-    fun provideUserApi(retrofit: Retrofit): UserApi = retrofit.create(UserApi::class.java)
-
-    @Provides
-    @Singleton
-    fun provideMediaApi(retrofit: Retrofit): MediaApi = retrofit.create(MediaApi::class.java)
-
-    @Provides
-    @Singleton
-    fun provideJobApi(retrofit: Retrofit): JobApi = retrofit.create(JobApi::class.java)
-
-    @Provides
-    @Singleton
-    fun provideDatabase(@ApplicationContext context: Context): AppDatabase =
-        Room.databaseBuilder(context, AppDatabase::class.java, "app.db")
-            .fallbackToDestructiveMigration()
-            .build()
-
-    @Provides
-    @Singleton
-    fun providePostRepository(
-        postApi: PostApi,
-        userApi: UserApi,
-        mediaApi: MediaApi,
-        db: AppDatabase
-    ): PostRepository = PostRepositoryImpl(postApi, userApi, db.postDao(), mediaApi,db)
-
-    @Provides
-    @Singleton
-    fun provideEventRepository(
-        eventApi: EventApi,
-        mediaApi: MediaApi,
-        db: AppDatabase
-    ): EventRepository = EventRepositoryImpl(eventApi, db.eventDao(), mediaApi)
-
-    @Provides
-    @Singleton
-    fun provideUserRepository(
-        userApi: UserApi,
-        db: AppDatabase,
-        postRepository: PostRepository
-    ): UserRepository =
-        UserRepositoryImpl(userApi, db.userDao(), postRepository)
-
-    @Provides
-    @Singleton
-    fun provideAuthRepository(
-        authApi: AuthApi,
-        tokenProvider: TokenProvider,
-        @ApplicationContext context: Context
-    ): AuthRepository = AuthRepositoryImpl(authApi, tokenProvider, context)
-
-    @Provides
-    @Singleton
-    fun provideJobRepository(jobApi: JobApi): JobRepository = JobRepositoryImpl(jobApi)
-
-    @Provides
-    @Singleton
-    fun provideParticipantAdapter(): ParticipantAdapter = ParticipantAdapter()
-
-    @Provides
-    @Singleton
-    fun provideEventAdapter(): EventAdapter = EventAdapter()
-
-    @Provides
-    @Singleton
-    fun provideUserAdapter(): UserAdapter = UserAdapter()
 }
